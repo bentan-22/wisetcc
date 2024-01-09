11c927e6e5590798b0c4f6a9d88e6a138c0593c3
Ben Tan <benjaminthtan@gmail.com>
2024-01-10 00:16:35 +0800

    add social media icons

--- wisetcc-frontend/src/components/Navbar.js
+++ wisetcc-frontend/src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaFacebook, FaInstagram, FaTwitterSquare } from 'react-icons/fa';
 import '../App.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="nav-links">
+      <div className="centered-links">
         <Link to="/" className="nav-item">Home</Link>
         <Link to="/about" className="nav-item">About</Link>
         <div className="nav-wisetcc">WISETCC</div>
         <Link to="/services" className="nav-item">Services</Link>
         <Link to="/contact" className="nav-item">Contact</Link>
+      </div>
+      <div className="social-icons">
+        <FaFacebook className="social-item" />
+        <FaInstagram className="social-item" />
+        <FaTwitterSquare className="social-item" />
       </div>
     </nav>
   );
