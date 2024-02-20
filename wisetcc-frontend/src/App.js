09fe0ab13c77626790734c531d4d3d79e74cd152
Ben Tan <benjaminthtan@gmail.com>
2024-02-20 18:48:39 +0800

    fix github pages issue

--- wisetcc-frontend/src/App.js
+++ wisetcc-frontend/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ServicesPage from './pages/ServicesPage';
-import ContactPage from './pages/ContactPage';
-import AppointmentPage from './pages/AppointmentPage';
-import ProductsPage from './pages/ProductsPage';
-import ProductA from './components/products/ProductA';
-import ProductB from './components/products/ProductB';
-import ProductC from './components/products/ProductC';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ServicesPage from "./pages/ServicesPage";
+import ContactPage from "./pages/ContactPage";
+import AppointmentPage from "./pages/AppointmentPage";
+import ProductsPage from "./pages/ProductsPage";
+import ProductA from "./components/products/ProductA";
+import ProductB from "./components/products/ProductB";
+import ProductC from "./components/products/ProductC";
 
 function App() {
+  const isGitHubPages = window.location.hostname.includes("github.io");
+  const basename = isGitHubPages ? "/wisetcc" : "";
   return (
-    <Router>
+    <Router basename={basename}>
       <div>
         <Navbar />
         <Routes>
